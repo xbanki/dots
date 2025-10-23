0caaf5b902b76473816c157797d05208aed67169
xbanki <contact@xbanki.me>
2025-10-23 04:27:20 +0000

    feat: Introduce zsh as login shell

---
+++ mod/zsh.nix
@@ -0,0 +1,30 @@
+#   Z Shell configuration
+#
+# Enables the usage of Z Shell (zsh) in the home configuration.
+#
+#   Copyright: Banki <contact@xbanki.me>
+#              Licensed under the MIT License.
+#              See LICENSE for details.
+
+
+{ ... }:
+
+{
+  programs.zsh = {
+    localVariables = {
+      EDITOR = "nvim";
+    };
+
+    shellAliases = {
+      mkdir = "mkdir -pv";
+      cp = "cp -irv";
+      ls = "ls -AlU";
+      rm = "rm -fir";
+      mv = "mv -iv";
+    };
+
+    history.append = true;
+    enable = true;
+    autocd = true;
+  };
+}
