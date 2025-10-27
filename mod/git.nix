022f978cba364d77446b2b15463863b7b6c40452
xbanki <contact@xbanki.me>
2025-10-27 02:04:47 +0000

    feat: Initialize Git configuration

---
+++ mod/git.nix
@@ -0,0 +1,11 @@
+#   Git configuration
+#
+# Very basic git configuration. Does not set any advanced properties.
+#
+#   Copyright: Banki <contact@xbanki.me>
+#              Licensed under the MIT License.
+#              See LICENSE for details.
+
+{ props, ... }:
+
+{ }
