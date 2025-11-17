f222e2ea0a8aa2978fe973f6af5e42975a581e91
xbanki <contact@xbanki.me>
2025-11-17 19:46:58 +0000

    feat: Add treesitter support

---
+++ mod/neovim/treesitter.nix
@@ -0,0 +1,37 @@
+#   Treesitter Configuration `github:nvim-treesitter/nvim-treesitter`
+#
+# 
+#
+#   Copyright: Banki <contact@xbanki.me>
+#              Licensed under the MIT License.
+#              See LICENSE for details.
+
+{ pkgs, ... }:
+
+{
+  plugins = {
+    treesitter-context.enable = true;
+    treesitter = {
+      enable = true;
+      settings = {
+        incremental_selection = {
+          enable = true;
+        };
+
+        grammarPacswapkages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+        textobjects.enable = true;
+        highlight.enable = true;
+        nixvimInjections = true;
+        indent.enable = true;
+      };
+    };
+
+    treesitter-textobjects = {
+      enable = true;
+      select = {
+        lookahead = true;
+        enable = true;
+      };
+    };
+  };
+}
