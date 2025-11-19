e1236b122b49257e72ac54a6fceb50e46eac7177
xbanki <contact@xbanki.me>
2025-11-19 13:38:17 +0000

    fix: Invalid Telescope configuration & broken indentation

--- mod/neovim/telescope.nix
+++ mod/neovim/telescope.nix
@@ -19,88 +19,84 @@
       fzf-native.enable = true;
       file-browser = {
         enable = true;
-	settings = {
-	  display_stat = let
+        settings = {
+          display_stat = let
             stat = {
-	      display = lib.nixvim.mkRaw "function(_) return '' end";
-	      width = 0;
-	    };
+              display = lib.nixvim.mkRaw "function(_) return '' end";
+              width = 0;
+            };
 
-	  in {
+          in {
             size = {
               display = lib.nixvim.mkRaw ''
-	        function(opts)
-		  if opts.stat.size ~= 0 then
-		    local m = "telescope._extensions.file_browser.fs_stat"
-		    return require(m).size.display(opts)
-		  end
+                function(opts)
+                  if opts.stat.size ~= 0 then
+                    local m = "telescope._extensions.file_browser.fs_stat"
+                    return require(m).size.display(opts)
+                  end
 
-		  return nil
-		end
-	      '';
-	    };
+                  return nil
+                end
+              '';
+            };
 
-	    date = stat;
-	    mode = stat;
-	  };
+            date = stat;
+            mode = stat;
+          };
 
-	  prompt_prefix = " ";
-	  hijack_netrw = true;
-	  git_status = false;
-	  theme = "dropdown";
-	  previewer = false;
-	  grouped = true;
-	  hidden = true;
-	};
+          prompt_prefix = " ";
+          hijack_netrw = true;
+          git_status = false;
+          theme = "dropdown";
+          previewer = false;
+          grouped = true;
+          hidden = true;
+        };
       };
     };
 
     settings = {
       pickers = {
-
         grep_string = {
-	  layout_config = {
-	    width = 0.56;
-	  };
+          layout_config.width = 0.56;
+          prompt_title = false;
+          prompt_prefix = "";
+          theme = "cursor";
+        };
 
-	  prompt_title = false;
-	  prompt_prefix = "";
-	  theme = "cursor";
-	};
-
-	live_grep = {
-	  layout_config = {
-	    preview_height = 0.60;
-	    height = 0.64;
+        live_grep = {
+          layout_config = {
+            preview_height = 0.60;
+            height = 0.64;
             width = 0.48;
-	  };
+          };
 
-	  layout_strategy = "vertical";
-	  prompt_title = false;
-	};
+          layout_strategy = "vertical";
+          prompt_title = false;
+        };
 
         current_buffer_fuzzy_find = {
-	  prompt_title = false;
-	  theme = "dropdown";
-	  previewer = false;
-	};
+          prompt_title = false;
+          theme = "dropdown";
+          previewer = false;
+        };
 
         find_files.layout_config = {
-	  layout_config.prompt_position = "bottom";
-	  preview_width = 0.60;
-	};
+          prompt_position = "bottom";
+          preview_width = 0.60;
+        };
       };
 
       defaults = {
         file_ignore_patterns = [
-	  "^node_modules/"
-	  "^%.mypy_cache/"
-	  "^__pycache__/"
-	  "%.ipynb"
-	  "^vendor/"
-	  "^build/"
-	  "^venv/"
-	  "^env/"
+          "^node_modules/"
+          "^%.mypy_cache/"
+          "^__pycache__/"
+          "%.ipynb"
+          "^vendor/"
+          "^build/"
+          "^venv/"
+          "^env/"
           "%.git"
         ];
 
