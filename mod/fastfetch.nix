051ad5d4d378558c913f0b35d05448ff2a63519c
xbanki <contact@xbanki.me>
2025-12-10 16:12:13 +0000

    feat: Introduce fastfetch

---
+++ mod/fastfetch.nix
@@ -0,0 +1,11 @@
+#   Fastfetch Configuration
+#
+#   Copyright: Banki <contact@xbanki.me>
+#              Licensed under the MIT License.
+#              See LICENSE for details.
+
+{ ... }:
+
+{
+  programs.fastfetch.enable = true;
+}
