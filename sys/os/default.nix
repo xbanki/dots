35828329b0d4a3e355e570f9df88bb7827dea425
xbanki <development@xbanki.me>
2026-07-02 11:41:38 +0300

    feat: Add system keyboard, timezone and local config

--- sys/os/default.nix
+++ sys/os/default.nix
@@ -9,21 +9,67 @@
 {
   version,
   inputs,
+  props,
   ...
 }:
 
 let
   system = "x86_64-linux";
+  pkgs = import inputs.nixpkgs {
+    config = {
+      allowUnfreePredicate = _: true;
+      allowUnfree = true;
+    };
+
+    inherit system;
+  };
+
   specialArgs = { inherit system; };
 
 in
 with inputs;
 nixpkgs.lib.nixosSystem {
   inherit specialArgs system;
-  modules = [
+  modules = with props; [
     ./boot.nix
     ./hardware.nix
     {
+      environment.sessionVariables = rec {
+        XKB_DEFAULT_LAYOUT = os.keyboardLayout;
+        GTK_IM_MODULE = INPUT_METHOD;
+        QT_IM_MODULE = INPUT_METHOD;
+        XMODIFIERS = "@im=fcitx";
+        INPUT_METHOD = "fcitx5";
+      };
+
+      console.useXkbConfig = true;
+      i18n = {
+        inputMethod = {
+          fcitx5 = {
+            waylandFrontend = true;
+            addons = with pkgs; [
+              fcitx5-gtk
+            ];
+          };
+
+          type = "fcitx5";
+          enable = true;
+        };
+
+        defaultLocale = os.locale;
+      };
+
+      services.xserver = {
+        xkb.layout = os.keyboardLayout;
+        exportConfiguration = true;
+      };
+
+      networking = {
+        networkmanager.enable = true;
+        hostName = os.hostname;
+      };
+
+      time.timeZone = os.timezone;
       system.stateVersion = version;
       nixpkgs.config = {
         allowUnfreePredicate = _: true;
