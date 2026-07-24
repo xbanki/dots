b3ee4c7efe409bdeaf97fd16ba86283116db152d
xbanki <development@xbanki.me>
2026-07-24 15:06:21 +0300

    feat: Update hardware spec

--- sys/os/hardware.nix
+++ sys/os/hardware.nix
@@ -6,12 +6,10 @@
   modulesPath,
   system,
   config,
-  props,
   lib,
   ...
 }:
 
-with props;
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -19,7 +17,7 @@ with props;
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/50f039aa-0f5d-41cf-a322-9e1a197f78d7";
+      device = "/dev/disk/by-uuid/4ff24e58-c219-4984-99fa-a8c853c9a277";
       fsType = "ext4";
     };
 
@@ -29,13 +27,8 @@ with props;
         "dmask=0022"
       ];
 
-      device = "/dev/disk/by-uuid/7FA9-D626";
+      device = "/dev/disk/by-uuid/1AF2-81C6";
       fsType = "vfat";
-    };
-
-    "/cache" = {
-      device = "/dev/disk/by-uuid/885fd50f-a5d1-4bcb-978f-cbd1bf733f0a";
-      fsType = "ext4";
     };
 
     "/share/ssd" = {
@@ -48,7 +41,7 @@ with props;
         "umask=000"
       ];
 
-      device = "/dev/disk/by-uuid/18BA252054E697DE";
+      device = "/dev/disk/by-uuid/8C0244B00244A156";
       fsType = "ntfs3";
     };
 
@@ -62,13 +55,27 @@ with props;
         "umask=000"
       ];
 
-      device = "/dev/disk/by-uuid/470C84C86E663F40";
+      device = "/dev/disk/by-uuid/4840454F40454542";
+      fsType = "ntfs3";
+    };
+
+    "/share/media" = {
+      options = [
+        "uid=1000"
+        "gid=1000"
+        "rw"
+        "user"
+        "exec"
+        "umask=000"
+      ];
+
+      device = "/dev/disk/by-uuid/7CC6671EC666D7C2";
       fsType = "ntfs3";
     };
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/517d82fc-1c14-4a63-b7f6-29b75f33311d"; }
+    { device = "/dev/disk/by-uuid/63d63beb-4c11-42ef-8751-f5014f572530"; }
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
@@ -90,15 +97,5 @@ with props;
       enable32Bit = true;
       enable = true;
     };
-  };
-
-  home-manager.users.${user.name}.xdg.cacheHome = "/cache/user/${user.name}";
-  systemd = {
-    user.settings.Manager.DefaultEnvironment = "XDG_CACHE_HOME=/cache/user/${user.name}";
-    services.nix-daemon.environment.XDG_CACHE_HOME = "/cache";
-    tmpfiles.rules = [
-      "d /cache/nix 0755 root root -"
-      "d /cache/user/${user.name} 0755 ${user.name} users -"
-    ];
   };
 }
